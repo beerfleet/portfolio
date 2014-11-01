7dbd61199458a48416e8bf300fda92e1ac6fa51e
Beerfleet <devel.janvb@gmail.com>
2014-11-01 20:42:40 +0100

    Slider finished

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 $(function () {
   init();
+  slider();
 });
 
 function init() {
@@ -23,28 +24,32 @@ function clearSelected() {
   $('#varia-btn p').removeClass('btn-selected');
 }
 
-function addButtonHandlers() {    
-  $("#personalia-btn").on('click', function() {
+function addButtonHandlers() {
+  $("#personalia-btn").on('click', function () {
     clearSelected();
     $('#personalia-btn p').addClass('btn-selected');
     hideAllBio();
     $("#personalia-content").fadeIn("slow")();
   });
-  
-  $("#sterke-punten-btn").on('click', function() {
+
+  $("#sterke-punten-btn").on('click', function () {
     clearSelected();
     $('#sterke-punten-btn p').addClass('btn-selected');
     hideAllBio();
     $("#sterke-punten-content").fadeIn("slow");
   });
-  
-  $("#varia-btn").on('click', function() {
+
+  $("#varia-btn").on('click', function () {
     clearSelected();
     $('#varia-btn p').addClass('btn-selected');
     hideAllBio();
     $("#varia-content").fadeIn("slow");
   });
-  
-  
 }
-  
+
+//slider
+function slider() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+}
