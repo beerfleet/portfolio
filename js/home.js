99a6cd567a8dde0dcc144c1ebb170aa50d5236e3
Beerfleet <devel.janvb@gmail.com>
2014-11-01 14:18:08 +0100

    Bio af

--- js/home.js
+++ js/home.js
@@ -13,14 +13,12 @@ function init() {
 function hideAllBio() {
   //$("#personalia-content").hide();
   $("#personalia-content").hide();
-  $("#over-mezelf-content").hide();
   $("#sterke-punten-content").hide();
   $("#varia-content").hide();
 }
 
 function clearSelected() {
   $('#personalia-btn p').removeClass('btn-selected');
-  $('#over-mezelf-btn p').removeClass('btn-selected');
   $('#sterke-punten-btn p').removeClass('btn-selected');
   $('#varia-btn p').removeClass('btn-selected');
 }
@@ -31,13 +29,6 @@ function addButtonHandlers() {
     $('#personalia-btn p').addClass('btn-selected');
     hideAllBio();
     $("#personalia-content").fadeIn("slow")();
-  });
-  
-  $("#over-mezelf-btn").on('click', function() {
-    clearSelected();
-    $('#over-mezelf-btn p').addClass('btn-selected');
-    hideAllBio();
-    $("#over-mezelf-content").fadeIn("slow");
   });
   
   $("#sterke-punten-btn").on('click', function() {
